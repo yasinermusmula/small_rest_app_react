d4e6537ab1cbec896ae3d3a7763d86da4031416b
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-03-24 18:14:05 +0300

    For like svg, color change defined wit useState hook

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -5,9 +5,14 @@ import {Link} from "react-router-dom";
 export default function Post(props) {
 
     const [expanded, SetExpanded] = useState(false);
+    const [color, SetColor] = useState(false)
 
     const handleExpandedClick = () => {
         SetExpanded(!expanded)
+    }
+
+    const changeLikeIconCollor = () => {
+        SetColor(!color)
     }
 
     const {title, text, userId, userName} = props;
@@ -40,7 +45,9 @@ export default function Post(props) {
                         <button
                             className="mr-3 p-2 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-500"
                             aria-label="Add to favorites">
-                            <svg className="h-6 w-6 text-gray-600" fill="currentColor" viewBox="0 0 24 24">
+                            <svg onClick={changeLikeIconCollor}
+                                 className={`h-6 w-6 ${color ? "text-red-500" : "text-gray-600"}`} fill="currentColor"
+                                 viewBox="0 0 24 24">
                                 <path fillRule="evenodd"
                                       d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
                                       clipRule="evenodd"/>
